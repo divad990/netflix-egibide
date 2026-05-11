365cb3781709419b3c23f28e8a36971054bf6606
divad990 <div.divad990@gmail.com>
2026-05-11 21:39:16 +0200

    Creadas migraciones seeders controladores y middlewares

---
+++ database/migrations/2026_05_11_193307_create_contents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->enum('type', ['movie', 'series']);
+            $table->string('image')->nullable();
+            $table->string('file_url')->nullable();
+            $table->year('year');
+            $table->foreignId('category_id')
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contents');
+    }
+};
